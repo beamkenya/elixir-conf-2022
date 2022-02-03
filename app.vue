182f716cdf79b9d50c2746133f07bec3570cbcf5
Magak Emmanuel <emashmagak@gmail.com>
2022-02-03 11:00:43 +0300

    added tailwind css

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p class="text-red-100 font-bold text-3xl bg-black underline">Test</p>
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  css: [
+    '@/assets/css/main.css',
+  ],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      }
+    },
+  }
 })
