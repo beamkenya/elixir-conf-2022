580ffe7a160e5488da67c05b0c02f1be859b92d7
Magak Emmanuel <emashmagak@gmail.com>
2022-06-23 18:15:19 +0300

    generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  target: 'static',
   router: {
     base: '/elixir-conf-2022/',
   },
