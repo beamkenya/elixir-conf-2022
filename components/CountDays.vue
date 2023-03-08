e921a6510b3a461f9df2d63b8bf96949d7672e01
Magak Emmanuel <emashmagak@gmail.com>
2023-03-08 16:48:13 +0300

    updates

--- components/CountDays.vue
+++ components/CountDays.vue
@@ -32,7 +32,7 @@ export default {
   components: { VueCountdown },
   data () {
     const now = new Date()
-    const d = new Date('2022-10-7')
+    const d = new Date('2023-05-26')
 
     return {
       time: d - now,
